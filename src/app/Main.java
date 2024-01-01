784e8fae859b6bac14e9db3a088769f8b1e1b339
bezshliaha <bezshliaha@gmail.com>
2024-01-01 18:23:21 +0100

    Add kilometer to mile

--- src/app/Main.java
+++ src/app/Main.java
@@ -6,15 +6,25 @@ public class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
-        System.out.println("Choose conversion option:\n1. Miles to Kilometers");
-        int choice = scanner.nextInt();
+        int choice;
+        do {
+            System.out.println("Choose the conversion option:\n1. " +
+                               "Miles to kilometers\n2. Kilometers to miles\n");
+            choice = scanner.nextInt();
 
-        if (choice == 1) {
-            double miles = Converter.getInput();
-            Converter.displayResult(miles, "miles", "kilometers");
-        } else {
-            System.out.println("Invalid choice.");
-        }
+            switch (choice) {
+                case 1:
+                    double miles = Converter.getInput();
+                    Converter.displayResult(miles, "miles", "kilometers");
+                    break;
+                case 2:
+                    double kilometers = Converter.getInput();
+                    Converter.displayResult(kilometers, "kilometers", "miles");
+                    break;
+                default:
+                    System.out.println("Invalid choice. Please choose again.");
+            }
+        } while (choice < 1 || choice > 2);
 
         scanner.close();
     }
